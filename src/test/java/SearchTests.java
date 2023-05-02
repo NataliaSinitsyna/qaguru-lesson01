48b68e26694d5e8f4ae2293c9384ff03cf359c3d
Natalia Sinitsyna <natalia.a.sinitsyna@gmail.com>
2023-05-02 21:49:53 +0300

    add configuration

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave( text("https://ru.selenide.org"));
+    }
+}
